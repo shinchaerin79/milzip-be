aa31b704689dfc125202b094bee7a2ab7bd12446
shinchaerin79 <shinchaerin79@gmail.com>
2026-05-12 13:09:29 +0900

    :tada: Start: Milzip 프로젝트 초기 세팅

---
+++ src/main/java/org/sku/milzip/MilzipApplication.java
@@ -0,0 +1,13 @@
+package org.sku.milzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilzipApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MilzipApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/sku/milzip/MilzipApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sku.milzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilzipApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
