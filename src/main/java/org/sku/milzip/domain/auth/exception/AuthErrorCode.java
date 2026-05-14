4970dfd3ad17d0947eb99cb7e43349b8dcbb0ebf
shinchaerin79 <shinchaerin79@gmail.com>
2026-05-14 11:41:09 +0900

    :sparkles: Feat: AuthErrorCode 추가

---
+++ src/main/java/org/sku/milzip/domain/auth/exception/AuthErrorCode.java
@@ -0,0 +1,30 @@
+package org.sku.milzip.domain.auth.exception;
+
+import org.sku.milzip.global.exception.model.BaseErrorCode;
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthErrorCode implements BaseErrorCode {
+
+  // 400 Bad Request
+  INVALID_CREDENTIALS("AUTH4001", "아이디 또는 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
+  DUPLICATE_EMAIL("AUTH4002", "이미 사용 중인 이메일입니다.", HttpStatus.BAD_REQUEST),
+
+  // 401 Unauthorized
+  AUTHENTICATION_REQUIRED("AUTH4011", "인증이 필요합니다.", HttpStatus.UNAUTHORIZED),
+  UNAUTHORIZED_TOKEN("AUTH4012", "유효하지 않은 토큰입니다.", HttpStatus.UNAUTHORIZED),
+  EXPIRED_ACCESS_TOKEN("AUTH4013", "만료된 액세스 토큰입니다. 리프레시가 필요합니다.", HttpStatus.UNAUTHORIZED),
+  EXPIRED_REFRESH_TOKEN("AUTH4014", "만료된 리프레시 토큰입니다. 재로그인이 필요합니다.", HttpStatus.UNAUTHORIZED),
+  INVALID_REFRESH_TOKEN("AUTH4015", "유효하지 않은 리프레시 토큰입니다.", HttpStatus.UNAUTHORIZED),
+
+  // 404 Not Found
+  MEMBER_NOT_FOUND("AUTH4041", "존재하지 않는 회원입니다.", HttpStatus.NOT_FOUND);
+
+  private final String code;
+  private final String message;
+  private final HttpStatus status;
+}
